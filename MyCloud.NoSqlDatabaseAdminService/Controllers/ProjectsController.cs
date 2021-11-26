1c8779a11922dce1f98a9dc402a786a95aeac872
RobinTTY <muellerobin95@gmail.com>
2021-11-26 16:18:26 +0100

    Initial commit

---
+++ MyCloud.NoSqlDatabaseAdminService/Controllers/ProjectsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCloud.NoSqlDatabaseAdminService.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[ApiVersion("1.0")]
+public class ProjectsController : ControllerBase
+{
+    private readonly ILogger<ProjectsController> _logger;
+
+    public ProjectsController(ILogger<ProjectsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetProjects")]
+    public string Get()
+    {
+        return "wow";
+    }
+}
---
+++ MyCloud.NoSqlDatabaseAdminService/Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1.0",
+        Title = "MyCloud - NoSql Database Admin Service",
+        Description = "The MyCloud - NoSql Database Admin Service provides a database service for modern app development." +
+                      "Enjoy elastic scalability while automating time‐consuming database admin tasks",
+        Contact = new OpenApiContact
+        {
+            Name = "Contact",
+            Email = "romuit04@hs-esslingen.de",
+            Url = new Uri("https://github.com/RobinTTY/MyCloud")
+        }
+    });
+});
+
+builder.Services.AddApiVersioning(config =>
+{
+    config.DefaultApiVersion = new ApiVersion(1, 0);
+    config.AssumeDefaultVersionWhenUnspecified = true;
+    config.ReportApiVersions = true;
+    config.ApiVersionReader = new HeaderApiVersionReader("api-version");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
