3e42238596c6eaefee6ee1db32ab3242efab4dbf
RobinTTY <muellerobin95@gmail.com>
2021-11-26 22:07:46 +0100

    Add project model and basic data store

---
+++ MyCloud.NoSqlDatabaseAdminService/Core/ApiContext.cs
@@ -0,0 +1,25 @@
+﻿using MyCloud.NoSqlDatabaseAdminService.Models;
+
+namespace MyCloud.NoSqlDatabaseAdminService.Core
+{
+    public interface IApiContext
+    {
+        void PopulateWithDemoData();
+    }
+
+    public class ApiContext : IApiContext
+    {
+        public List<Project> Projects { get; set; }
+
+        public ApiContext()
+        {
+            Projects = new List<Project>();
+        }
+
+
+        public void PopulateWithDemoData()
+        {
+            Projects.Add(new Project("VideoStreamPro"));
+        }
+    }
+}
---
+++ MyCloud.NoSqlDatabaseAdminService/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿namespace MyCloud.NoSqlDatabaseAdminService.Models;
+
+public class Project
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public DateTime CreatedDate { get; set; }
+
+    public Project(string name)
+    {
+        Id = Guid.NewGuid();
+        CreatedDate = DateTime.Now;
+        Name = name;
+    }
+}
