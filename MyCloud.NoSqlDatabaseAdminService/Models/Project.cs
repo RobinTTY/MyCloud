69c6a7b516c33b1a15e113485ef9d49d0b2e1419
RobinTTY <muellerobin95@gmail.com>
2021-11-29 18:43:15 +0100

    Add documentation to project model

--- MyCloud.NoSqlDatabaseAdminService/Models/Project.cs
+++ MyCloud.NoSqlDatabaseAdminService/Models/Project.cs
@@ -2,9 +2,21 @@
 
 public class Project
 {
+    /// <summary>
+    /// The unique id of the project.
+    /// </summary>
     public Guid Id { get; set; }
+    /// <summary>
+    /// The name of the project.
+    /// </summary>
     public string Name { get; set; }
+    /// <summary>
+    /// The project description.
+    /// </summary>
     public string Description { get; set; }
+    /// <summary>
+    /// Timestamp of when the project was created.
+    /// </summary>
     public DateTime CreatedDate { get; set; }
 
     public Project(string name, string description = "")
